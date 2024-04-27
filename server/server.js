5dff7c6d7bd5f46c4546b729b2b5e644a2016380
amitsaxena93782 <70472299+amitsaxena93782@users.noreply.github.com>
2024-04-27 22:11:24 +0200

    Started

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    console.log('Hello World');
+});
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
+
+
